71af0e0a02dac99b5bae18f5e5d8c339aefc3f86
Wen Fang Tai <109420213+WenFung@users.noreply.github.com>
2024-12-13 11:35:17 +0800

    因為新增CourseProfessor表格所以調整API

--- controller/comment.py
+++ controller/comment.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database.database import SessionLocal
 from sqlalchemy import func
-from database.models import User, Professor, Comment, Course
+from database.models import User, College, Professor, Comment, Course, CourseProfessor
 
 router = APIRouter()
 
@@ -17,13 +17,16 @@ async def get_info_in_course(course_name: str):
         results = (
             db.query(
                 Course.name.label("course_name"),
-                Course.id.label("course_id"),
                 Course.course_info.label('course_info'),
                 Course.course_year.label('course_year'),
-                Professor.name.label("professor_name")
+                Professor.name.label("professor_name"),
+                College.department_name.label("department_name")
             )
             .select_from(Course)  # 選擇 Course 表
-            .join(Professor, Professor.id == Course.professor_id)  # 關聯 Professor 和 Course
+            #.join(Professor, Professor.id == Course.professor_id)  # 關聯 Professor 和 Course
+            .join(CourseProfessor, CourseProfessor.course_id == Course.id)  # 相聯 Course 和 CourseProfessor
+            .join(Professor, Professor.id == CourseProfessor.professor_id)  # 相聯 Professor 和 CourseProfessor
+            .join(College, College.department_id == Course.department_id)  # 相聯 College 和 Course
             .filter(
                 Course.name.contains(course_name),
             )
@@ -31,17 +34,27 @@ async def get_info_in_course(course_name: str):
         )
 
         # 整理查詢結果為結構化的列表
-        courses_details = []
+        courses_details = {}
         for result in results:
-            courses_details.append({
-                "course_name": result.course_name,
-                "professor_name": result.professor_name,
-                "course_id": result.course_id,
-                "course_info": result.course_info,
-                "course_year": result.course_year
-            })
+            course_name = result.course_name
+            department_name = result.department_name
+            course_info = result.course_info
+            course_year = result.course_year
+            comment_count = result.count or 0  # 如果沒有評論，默認為 0
 
-        return courses_details
+            if course_name not in courses_details:
+                courses_details[course_name] = {
+                    "professor_name": [],
+                    "department_name": department_name,
+                    "course_info": course_info,
+                    "course_year": course_year
+                }
+            if result.professor_name:
+                courses_details[course_name]["professor_name"].append(result.professor_name)
+        
+        # 將字典轉換為列表以返回
+        courses_list = list(courses_details.values())
+        return courses_list
 
     except Exception as e:
         print(f"Error occurred in get_courses_with_professors: {e}")
@@ -65,10 +78,11 @@ async def get_comment_in_course(course_name: str):
                 User.chineseName.label("chinesename"),
                 Professor.name.label("professor_name")
             )
-            .select_from(Comment)  # 選擇 Course 表
-            .join(Course, Course.id == Comment.course_id)  # 關聯 Professor 和 Course
-            .join(Professor, Professor.id == Course.professor_id)  # 關聯 Professor 和 Course
-            .join(User, User.studentId == Comment.user_id)  # 關聯 Professor 和 Course
+            .select_from(Course)  # 選擇 Course 表
+            .join(CourseProfessor, CourseProfessor.course_id == Course.id)  # 相聯 Course 和 CourseProfessor
+            .join(Professor, Professor.id == CourseProfessor.professor_id)  # 相聯 Professor 和 CourseProfessor
+            .join(Comment, Comment.course_id == Course.id)  # 關聯 Course 和 Comment
+            .join(User, User.studentId == Comment.user_id)  # 關聯 Comment 和 User
             .filter(
                 Course.name.contains(course_name),
             )
@@ -76,17 +90,29 @@ async def get_comment_in_course(course_name: str):
         )
 
         # 整理查詢結果為結構化的列表
-        courses_details = []
+        comment_details = {}
         for result in results:
-            courses_details.append({
-                "chinesename": result.chinesename,
-                "course_content": result.course_content,
-                "course_score": result.course_score,
-                "professor_name": result.professor_name,
-                "time": result.time
-            })
+            chinesename = result.chinesename
+            course_score = result.course_score or 0  # 如果沒有評論，默認為 0
+            course_content = result.course_content
+            time = result.time
 
-        return courses_details
+            if chinesename not in comment_details:
+                # 格式化時間，移除 T
+                formatted_time = result.time.strftime("%Y-%m-%d %H:%M:%S") if result.time else None
+                comment_details[chinesename] = {
+                    "chinesename": chinesename,
+                    "professor_name": [],
+                    "course_score": course_score,
+                    "course_content": course_content,
+                    "time": formatted_time
+                }
+            if result.professor_name:
+                comment_details[chinesename]["professor_name"].append(result.professor_name)
+        
+        # 將字典轉換為列表以返回
+        courses_list = list(comment_details.values())
+        return courses_list
 
     except Exception as e:
         print(f"Error occurred in get_courses_with_professors: {e}")
